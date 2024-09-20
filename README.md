8c18c35da9c9d4541e7abffcade312f53bf71601
David <goyaldavid55@gmail.com>
2024-09-20 09:58:56 +0530

    Updated README

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-﻿
-# MERN E-COMMERCE SERVER 2024
+﻿# SHOPIO
 
-Hi! My name is **David Goyal**, I have created this MERN stack website.
-
+This is a full stack ecommerce website built with MERN stack.
 
 # Install Dependencies
 
 **For Backend** - `npm i && npm run build`
 
-
 ## Env Variables
 
-Make Sure to Create a  .env file in root directory and add appropriate variables in order to use the app.
+Make Sure to Create a .env file in root directory and add appropriate variables in order to use the app.
 
 **Essential Variables**
 PORT= `4000 or any`
@@ -22,7 +19,6 @@ PRODUCT_PER_PAGE=`8 or any`
 _fill each filed with your info respectively_
 
 ## Author
-
 
 **LinkedIn** Click [Here](www.linkedin.com/in/david-goyal) **@DavidGoyal**
 **X** Click [Here](https://x.com/David__Goyal) **@DavidGoyal**
--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,53 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import morgan from "morgan";
 import { connectDB } from "./utils/features.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import NodeCache from "node-cache";
 import Stripe from "stripe";
-import cors from "cors"
+import cors from "cors";
 
-
-import userRoutes from "./routes/user.js"
-import productRoutes from "./routes/product.js"
-import orderRoutes from "./routes/order.js"
-import paymentRoutes from "./routes/payment.js"
-import dashboardRoutes from "./routes/stats.js"
-
+import userRoutes from "./routes/user.js";
+import productRoutes from "./routes/product.js";
+import orderRoutes from "./routes/order.js";
+import paymentRoutes from "./routes/payment.js";
+import dashboardRoutes from "./routes/stats.js";
 
 dotenv.config({
-    path:".env"
+	path: ".env",
 });
 
-const port=process.env.PORT || 3000;
-const stripeKey=process.env.STRIPE_KEY || "";
+const port = process.env.PORT || 3000;
+const stripeKey = process.env.STRIPE_KEY || "";
 
 connectDB();
 
-export const stripe=new Stripe(stripeKey);
-export const myCache=new NodeCache();
+export const stripe = new Stripe(stripeKey);
+export const myCache = new NodeCache();
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors({
-    origin:["http://localhost:5173","http://localhost:5174",process.env.FRONTEND_URL as string],
-}));
+app.use(
+	cors({
+		origin: [
+			"http://localhost:5173",
+			"http://localhost:5174",
+			process.env.FRONTEND_URL as string,
+		],
+	})
+);
 
-
-app.use("/api/v1/user",userRoutes);
-app.use("/api/v1/product",productRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/order", orderRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
-app.use("/uploads",express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
 
-
-app.listen(3000,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(3000, () => {
+	console.log(`Server is running on http://localhost:${port}`);
 });
